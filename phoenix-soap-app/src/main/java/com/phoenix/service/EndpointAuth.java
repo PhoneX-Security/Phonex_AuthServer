9acf44f73bf6bad893406c94875dad2851f210e0
Dusan Klinec <dusan.klinec@gmail.com>
2015-02-23 17:42:55 +0100

    Better IP address extraction from request.

--- phoenix-soap-app/src/main/java/com/phoenix/service/EndpointAuth.java
+++ phoenix-soap-app/src/main/java/com/phoenix/service/EndpointAuth.java
@@ -170,18 +170,30 @@ public class EndpointAuth {
      * @return 
      */
     public String getIp(HttpServletRequest request){
-        String ipAddress = null;
+        String ip = null;
         
         try {
-            ipAddress = request.getHeader("X-FORWARDED-FOR");
-            if (ipAddress == null) {
-                ipAddress = request.getRemoteAddr();
+            ip = request.getHeader("X-Forwarded-For");
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("Proxy-Client-IP");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_CLIENT_IP");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getRemoteAddr();
             }
         } catch(Exception e){
             
         }
         
-        return ipAddress;
+        return ip;
     }
     
     /**
